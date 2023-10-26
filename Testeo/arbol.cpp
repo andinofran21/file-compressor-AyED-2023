9c5d4803e2ab752ca743fd642760e1caf29546db
Franco Andino <francoarianandino@gmail.com>
2023-10-26 16:40:27 -0300

    Compressor2=>ADD: _mostrarArbol Funcion que muestra el codigo de cada hoja por pantalla FIX: Cambio List<HuffmanTreeInfo> por List<HuffmanTreeInfo*>

--- Testeo/arbol.cpp
+++ Testeo/arbol.cpp
@@ -8,7 +8,7 @@
 #include "../biblioteca/tads/BitWriter.hpp"
 #include "../biblioteca/tads/BitReader.hpp"
 #include "../biblioteca/tads/huffman/HuffmanSetup.hpp"
-#include "../compressor.hpp"
+#include "../compressor2.hpp"
 #include "../decompressor.hpp"
 
 // #include "../AlgoritmosAFondo_EMPTY_v2.7.18/src/biblioteca/tads/huffman/Progress.hpp"
@@ -27,13 +27,17 @@ int main()
 
     // ---------------------------------------------------------------------------
     // PASO 2: Crear lista
-    List<HuffmanTreeInfo> lista = list<HuffmanTreeInfo>();
+    List<HuffmanTreeInfo*> lista = list<HuffmanTreeInfo*>();
     crearLista(lista,tabla);
   
     // ---------------------------------------------------------------------------
     // PASO 3: Crear arbol
     HuffmanTreeInfo *tree = crearArbol(lista);
   
+
+    _mostrarArbol(tree);
+
+
     // Recorro el arbol para obtener los codigos y los cargo
     // en el array de contenedores, en el campo code
     cargarCodigoEnTabla(tree, tabla);
